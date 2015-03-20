4830275d27b0924459c109e38203426055d1dd95
JayLamb20 <jaylamb20@gmail.com>
2015-03-19 23:31:04 -0400

    started on task1

---
+++ Data_Science_Capstone/task1.R
@@ -0,0 +1,31 @@
+## Tokenization
+
+
+    ## read the data (start with blogs)
+    con <- file(".\\Coursera-SwiftKey\\final\\en_US\\en_US.blogs.txt", open="r")
+    data <- readLines(con, n = 4000)
+    close(con); rm(con);
+    
+    ## do some whitespace stripping
+    data <- stripWhitespace(data)
+    
+    ## preliminary frequency table
+    freqs <- termFreq(data)
+    
+    ## first pass (premade functions)
+    library(tm)
+    pass1a <- scan_tokenizer(data)
+    pass1b <- MC_tokenizer(data) ## returns more elements
+    
+## Profanity Filtering
+    
+    ## One approach (prebuilt function from tm package)
+    cleandata <- removeWords(x=data, words=c("the","bad","words"))
+    
+## Other Stuff
+    
+    ## find frequent terms
+    
+## References
+    
+    [1] http://stackoverflow.com/questions/12626637/reading-a-text-file-in-r-line-by-line
