d2e579ef86b21153e80e2609a2c1122d8471323d
JayLamb20 <jaylamb20@gmail.com>
2015-03-27 06:44:37 -0400

    started work on milestone report

--- Data_Science_Capstone/task1.R
+++ Data_Science_Capstone/task1.R
@@ -5,8 +5,10 @@
     library(qdap)
 
     ## read the data (start with blogs)
-    con <- file(".\\Coursera-SwiftKey\\final\\en_US\\en_US.blogs.txt", open="r")
-    data <- readLines(con, n = 4000)
+    url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+    
+    con <- file(".\\Coursera-SwiftKey\\final\\en_US\\en_US.blogs.txt", open="rb")
+    data <- readLines(con, n = 4000, encoding="UTF-8")
     close(con); rm(con);
     
     ## do some whitespace stripping
